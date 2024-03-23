05f97dcc54cf4a4687c11448d67c71c1f052d503
soo <126047958+sumiiiin@users.noreply.github.com>
2024-03-23 21:18:17 +0900

    Delete projects.zip-20240316T092708Z-001 directory

--- projects.zip-20240316T092708Z-001/.metadata/.plugins/org.eclipse.core.resources/.history/e1/008c462b3be0001e1f42ceb5c2722247
+++
@@ -1,5 +0,0 @@
-package ch08.sec12;
-
-public class InstanceofExample {
-
-}
--- projects.zip-20240316T092708Z-001/my_lib/src/pack2/B.java
+++
@@ -1,7 +0,0 @@
-package pack2;
-
-public class B {
-	public void method() {
-		System.out.println("B-method 실행");
-	}
-}
